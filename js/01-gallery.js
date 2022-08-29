7a4ba0606c02b79a5be5ed8b1ff2ac79e21ff7cb
Lluppiter <petya1411@gmail.com>
2022-08-29 15:30:06 +0300

    add js hw 2 fix 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-console.log(galleryItems);
-
 const galleryList = document.querySelector(".gallery");
-console.log(galleryList);
 
-const listImages = galleryItems.map(
-  (image) =>
-    `<a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`
-);
+const listImages = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`
+  )
+  .join("");
 console.log(listImages);
-listItems.insertAdjacentHTML("beforeend", listImages);
+galleryList.insertAdjacentHTML("beforeend", listImages);
