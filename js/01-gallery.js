d85955dbcc708628485755ac81d80f7707369ff6
Lluppiter <petya1411@gmail.com>
2022-08-29 22:42:52 +0300

    add js hw 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryList = document.querySelector(".gallery");
-
 const listImages = galleryItems
   .map(
     (image) =>
       `<a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`
   )
   .join("");
-console.log(listImages);
 galleryList.insertAdjacentHTML("beforeend", listImages);
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+  const closeModalByEsc = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+      console.log("отслеживает");
+      document.removeEventListener("keydown", closeModalByEsc);
+    }
+  };
+  document.addEventListener("keydown", closeModalByEsc);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const listImages = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`
+  )
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", listImages);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
